c5720717793dbbb2bfb8a833717f7ead205295be
Cathy Theys <cathy.theys@lullabot.com>
2020-11-16 17:01:06 -0500

    Oops. Fix script location.

--- scripts/lando/post-start.sh
+++ scripts/lando/post-start.sh
@@ -7,24 +7,24 @@ cd /app
 composer install --no-suggest --no-interaction --no-progress
 
 # Local setup instructions
-if test ! -d /app/.lando.local.env; then
+if test ! -f /app/.lando.local.env; then
   printf "\n\n*****************\n"
-  echo "Copying .example.lando.local.env to .lando.local.env\n"
-  echo "Edit this file to change the url drush uses\n"
+  echo "Copying .example.lando.local.env to .lando.local.env"
+  echo "Edit this file to change the url drush uses"
   cp /app/.example.lando.local.env /app/.lando.local.env
 fi
 
-if test ! -d /app/.lando.local.yml; then
+if test ! -f /app/.lando.local.yml; then
   printf "\n\n*****************\n"
-  echo "Copying .example.lando.local.yml to .lando.local.yml\n"
-  echo "Edit this file to turn on and off xdebug and set the php.ini and env location.\n"
+  echo "Copying .example.lando.local.yml to .lando.local.yml"
+  echo "Edit this file to turn on and off xdebug and set the php.ini and env location."
   cp /app/.example.lando.local.yml /app/.lando.local.yml
 fi
 
-if test ! -d /app/.lando.php.ini; then
+if test ! -f /app/.lando.php.ini; then
   printf "\n\n*****************\n"
-  echo "Copying .example.lando.php.ini to .lando.php.ini\n"
-  echo "Edit this file to configure ports or xdebug connect back.\n"
+  echo "Copying .example.lando.php.ini to .lando.php.ini"
+  echo "Edit this file to configure ports or xdebug connect back."
   cp /app/.example.lando.php.ini /app/.lando.php.ini
 fi
 
@@ -40,6 +40,5 @@ printf "Import db by running:\n"
 printf "lando db-import database.sql.gz\n\n"
 
 # Install instructions
-printf "\n\n*****************\n"
 printf "Or install using:\n"
 printf "lando drush site-install standard --db-url='mysql://[db_user]:[db_pass]@localhost/[db_name]' --site-name=Example\n\n"
